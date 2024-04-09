a1c3590d85a67d2967ca9d16fd874bb2a3eff322
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-04-09 09:36:27 +0900

    chore: add use Student method

--- src/main/oop/oop01/OopTask01.java
+++ src/main/oop/oop01/OopTask01.java
@@ -3,5 +3,7 @@ package oop01;
 public class OopTask01 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        var st = new Student(1, "ken");
+        st.print();
     }
 }
