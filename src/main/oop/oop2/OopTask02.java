ddaeeebfdc97deac45e491dc75db79f45182ca55
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-04-16 18:54:28 +0900

    feat: Task

---
+++ src/main/oop/oop2/OopTask02.java
@@ -0,0 +1,26 @@
+package oop2;
+
+public class OopTask02{
+    public static void main(String[] args){
+        for (int i = 0; i < 100; i++) {
+            Student st = new Student(10, "student");
+            System.out.println(st.toString());
+        }
+    }
+}
+
+class Student{
+    public int id;
+    public String name;
+    public static int count = 0;
+
+    public Student(int id, String name){
+        this.id = id;
+        this.name = name;
+        ++count;
+    }
+
+    public String toString() {
+        return "id: " + this.id + ", name: " + this.name + ", count: " + this.count;
+    }
+}
